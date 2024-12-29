7221779c9ec24a9bc2ab0123729b4bcc34bb2fa5
nhan.trinh <hongnhan50@gmail.com>
2024-12-30 00:14:37 +0700

    setup project in go

---
+++ main.go
@@ -0,0 +1,30 @@
+﻿package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg = config
+
+func main() {
+	a := app.New()
+	win := a.NewWindow("Daily_Task")
+	edit, preview := cfg.makeUI()
+
+}
+
+func (app *config) makeUI(*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+}
