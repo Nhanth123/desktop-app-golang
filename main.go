292cec4ebe979c2ad5201c1839bf27f95ff61911
nhan.trinh <hongnhan50@gmail.com>
2025-02-01 17:56:33 +0700

    Add doctor.go to project

--- main.go
+++ main.go
@@ -1,38 +1,16 @@
 ﻿package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"fmt"
+	"myapp/doctor"
 )
 
-type config struct {
-	EditWidget    *widget.Entry
-	PreviewWidget *widget.RichText
-	CurrentFile   fyne.URI
-	SaveMenuItem  *fyne.MenuItem
-}
-
-var cfg config
-
 func main() {
-	a := app.New()
-	win := a.NewWindow("Daily_Task")
-	edit, preview := cfg.makeUI()
-	win.SetContent(container.NewHSplit(edit, preview))
-
-	win.Resize(fyne.Size{Width: 800, Height: 500})
-	win.CenterOnScreen()
-	win.ShowAndRun()
+	var whatToSay string
+	whatToSay = doctor.Intro()
+	fmt.Println("What to say:", whatToSay)
 }
 
-func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
-	edit := widget.NewMultiLineEntry()
-	preview := widget.NewRichTextFromMarkdown("")
-	app.EditWidget = edit
-	app.PreviewWidget = preview
-
-	edit.OnChanged = preview.ParseMarkdown
-	return edit, preview
+func sayHello(s string) {
+	fmt.Println(s)
 }
