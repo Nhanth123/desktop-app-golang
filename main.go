a4874a6f12388409325c55c4a3a617aa4b9f807f
nhan.trinh <hongnhan50@gmail.com>
2025-02-03 16:57:45 +0700

    add dot notation in go

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 ﻿package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
-	var whatToSay string
-	whatToSay = doctor.Intro()
+	reader := bufio.NewReader(os.Stdin)
+	whatToSay := doctor.Intro()
 	fmt.Println("What to say:", whatToSay)
-}
 
-func sayHello(s string) {
-	fmt.Println(s)
+	for {
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		response := doctor.Response(userInput)
+
+		if userInput == "exit" || response == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+
+		fmt.Println(response)
+	}
 }
